46697750250ef90fc0b4d878f817fd72f088619c
Dallin Yauney <70169075+DallinYauney@users.noreply.github.com>
2026-03-31 00:45:20 -0600

    fix the indenting I somehow messed up (???)

--- cs2430Program3/src/cs2430Program3/Main.java
+++ cs2430Program3/src/cs2430Program3/Main.java
@@ -1,28 +1,27 @@
 package cs2430Program3;
 
 public class Main {
+    public static void main(String[] args) {
+        Experiment[] experiments = {
+            new Experiment("Cloud Patterns",            36,  5),
+            new Experiment("Solar Flares",              264, 9),
+            new Experiment("Solar Power",               188, 6),
+            new Experiment("Binary Stars",              203, 8),
+            new Experiment("Relativity",                104, 8),
+            new Experiment("Seed Viability",            7,   4),
+            new Experiment("Sun Spots",                 90,  2),
+            new Experiment("Mice Tumors",               65,  8),
+            new Experiment("Microgravity Plant Growth", 75,  5),
+            new Experiment("Micrometeorites",           170, 9),
+            new Experiment("Cosmic Rays",               80,  7),
+            new Experiment("Yeast Fermentation",        27,  4),
+        };
 
-	public static void main(String[] args) {
-            Experiment[] experiments = {
-                  new Experiment("Cloud Patterns",            36,  5),
-                  new Experiment("Solar Flares",              264, 9),
-                  new Experiment("Solar Power",               188, 6),
-                  new Experiment("Binary Stars",              203, 8),
-                  new Experiment("Relativity",                104, 8),
-                  new Experiment("Seed Viability",            7,   4),
-                  new Experiment("Sun Spots",                 90,  2),
-                  new Experiment("Mice Tumors",               65,  8),
-                  new Experiment("Microgravity Plant Growth", 75,  5),
-                  new Experiment("Micrometeorites",           170, 9),
-                  new Experiment("Cosmic Rays",               80,  7),
-                  new Experiment("Yeast Fermentation",        27,  4),
-            };
+        // DELETEME - Proof of concept for DP solution
+        Payload result = Dynamic.calculate(experiments, 700);
 
-            // DELETEME - Proof of concept for DP solution
-            Payload result = Dynamic.calculate(experiments, 700);
-
-            System.out.println(result.experimentList());
-            System.out.println(result.getWeight());
-            System.out.println(result.getRating());
-	}
+        System.out.println(result.experimentList());
+        System.out.println(result.getWeight());
+        System.out.println(result.getRating());
+    }
 }

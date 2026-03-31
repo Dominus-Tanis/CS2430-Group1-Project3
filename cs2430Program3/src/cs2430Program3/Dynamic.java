9f0e2bc4dbb10953a281a18d488adc713dde31b2
Dallin Yauney <70169075+DallinYauney@users.noreply.github.com>
2026-03-31 00:31:22 -0600

    clean up code

--- cs2430Program3/src/cs2430Program3/Dynamic.java
+++ cs2430Program3/src/cs2430Program3/Dynamic.java
@@ -2,70 +2,98 @@ package cs2430Program3;
 
 import java.util.ArrayList;
 
+/**
+ * Solution to the knapsack problem designed to be more efficient than
+ * brute force, but almost as good. Uses the ideas of Dynamic Programming
+ * to create a table and gradually add experiments to the table, keeping track
+ * of the best ones.
+ * Note that currently this is probably worse than brute force, since this operates in
+ * experiments * maxWeight = 12*700 = 8400 operations, over double that for brute force.
+ */
 public class Dynamic {
-    public static void main(String[] args) {
-		 Experiment[] experiments = {
-             new Experiment("Cloud Patterns",            36,  5),
-             new Experiment("Solar Flares",              264, 9),
-             new Experiment("Solar Power",               188, 6),
-             new Experiment("Binary Stars",              203, 8),
-             new Experiment("Relativity",                104, 8),
-             new Experiment("Seed Viability",            7,   4),
-             new Experiment("Sun Spots",                 90,  2),
-             new Experiment("Mice Tumors",               65,  8),
-             new Experiment("Microgravity Plant Growth", 75,  5),
-             new Experiment("Micrometeorites",           170, 9),
-             new Experiment("Cosmic Rays",               80,  7),
-             new Experiment("Yeast Fermentation",        27,  4),
-         };
-         Payload result = calculate(experiments, 700);
-
-    //    Experiment[] experiments = {
-    //        new Experiment("Sheet", 3, 4),
-    //        new Experiment("Box",   6, 5),
-    //        new Experiment("Bag",   5, 5),
-    //        new Experiment("Spec",  1, 1),
-    //    };
-    //    Payload result = calculate(experiments, 10);
-        
-        
-        System.out.println(result.experimentList());
-        System.out.println(result.getWeight());
-        System.out.println(result.getRating());
-    }
-
-    private static void printTable(int[][] table) {
-        for(int[] row : table) {
-            System.out.printf("\n[ ");
-            for(int cell : row) {
-                System.out.printf("%d\t", cell);
-            }
-            System.out.printf("]");
-        }
-        System.out.println("\n");
-    }
-
+    /**
+     * Entry method for the DP solution.
+     * Tries to find the best combination of
+     * experiments in O(experiment * maxWeight) space & time.
+     * @param experiments Array of experiments to potentially include
+     * @param maxWeight The maximum weight of the payload
+     * @return a Payload of the highest-rated experiments it could find
+     */
     public static Payload calculate(Experiment[] experiments, int maxWeight) {
+        // Create a DP table to track the best score for each weight, incrementally
+        // adding in new experiments as you go.
+        // Each row is a new experiment, and each column is a new weight.
         int[][] table = new int[experiments.length][maxWeight+1];
+        // note: it makes maxWeight+1 columns because column 0 corresponds to 0kg
 
+        // call addExperiment on each row
         for(int i=0; i<experiments.length; i++) {
             Experiment currentExperiment = experiments[i];
             addExperiment(table, i, currentExperiment);
         }
         
+        // It's not clear what experiments ended up being useful until the table is
+        // complete, so we trace back through the table seeing what experiments got
+        // used to determine what to return.
         ArrayList<Experiment> usedExperiments = traceExperiments(table, experiments);
-        printTable(table);
-        
         return new Payload(usedExperiments);
     }
 
+    /**
+     * Adds values to a row in the table. Each row corresponds to
+     * an experiment, so this method adds an experiment to the table using
+     * the values from the previous row.
+     * @param table DP table of experiments/weights/ratings
+     * @param row row of the experiment to calculate
+     * @param experiment corresponding Experiment object, to use its weight/rating
+     */
+    private static void addExperiment(int[][] table, int row, Experiment experiment) {
+        // This is the crux of the DP algorithm.
+        //
+        // For every weight, if the experiment is light enough to be considered, consider
+        // the rating from the previous experiments at currentWeight-experimentWeight. If
+        // using the current experiment would yield a better rating what we have for the
+        // previous experiments (at the current weight), then use it instead. 
+        for(int weight=0; weight<table[0].length; weight++) {
+
+            // edge case: if the current experiment is too heavy for the current weight, then
+            // just default to what we had for the previous experiments for the current weight
+            if(experiment.getWeight() > weight) {
+                table[row][weight] = getAbove(table, row, weight);
+                continue;
+            }
+
+            // subtract the current experiment's weight. How well did previous experiments
+            // do for that weight. If it wasn't very good, we might be able to add this experiment
+            // to it for the current weight to get a better rating
+            int prevScoreBack = getAbove(table, row, weight - experiment.getWeight());
+
+            // how well would we do combining the current experiment with prevScoreBack?
+            int candidateScore = prevScoreBack + experiment.getRating();
+
+            // How well did the previous experiments do for the current weight?
+            int prevScoreAbove = getAbove(table, row, weight);
+
+            // set the current weight's rating to be whichever one was better
+            table[row][weight] = Integer.max(candidateScore, prevScoreAbove);
+        }
+    }
+
+    /**
+     * Starting at the end of the table, work backwards to figure
+     * out what experiments were used to set that rating/weight combo.
+     * @param table DP table of experiments/weights/ratings
+     * @param experiments experiments for each row in the table
+     * @return the experiments that were used to make best rating in table
+     */
     private static ArrayList<Experiment> traceExperiments(int[][] table, Experiment[] experiments) {
         ArrayList<Experiment> output = new ArrayList<Experiment>();
 
-        // 1. start at bottom right
-        // 2. go left until you see a change. if 0, quit
-        // 3. go up until you see the thing that made that change. include it
-        // 4. goto 2.
+        // Overall structure:
+        // 1. start at bottom right of table
+        // 2. go left until you see a change. if 0, quit (done!)
+        // 3. go up until you see the experiment that made that change. include it
+        // 4. repeat from 2.
 
         int row = table.length-1;
         int col = table[0].length-1;
@@ -73,14 +101,18 @@ public class Dynamic {
             int cell = table[row][col];
             int leftCell = table[row][col-1];
 
+            // base case: if cell is 0, we've run out of
+            // experiments to add, so quit
             if(cell == 0) break;
 
+            // we only care if leftCell is different
             if(cell == leftCell) {
                 cell = table[row][--col];
                 continue;
             }
 
             // at this point, cell != leftCell
+            // go up until cell != the above cell
             if(row != 0) {
                 while(table[row][col] == table[row-1][col]) {
                     row--;
@@ -94,47 +126,50 @@ public class Dynamic {
             Experiment currentExperiment = experiments[row];
             output.add(experiments[row]);
 
+            // Success! we have the experiment that was used, now set the
+            // current cell to be the one currentExperiment was added onto
             row -= 1;
             col -= currentExperiment.getWeight();
-            if(row == -1) break;
 
+            // edge case: don't loop if this is the very first row
+            if(row == -1) break;
+            // note that, since the entire left column is 0's (0kg), we don't need
+            // a dedicated edge case for boundary on the left side, because the
+            // above base case takes care of that.
         }
 
         return output;
     }
 
-    private static void addExperiment(int[][] table, int row, Experiment experiment) {
-        for(int weight=0; weight<table[0].length; weight++) {
-            if(experiment.getWeight() > weight) {
-                table[row][weight] = above(table, row, weight);
-                continue;
-            }
-            int prevScoreBack = getPrevScore(table, row, weight, experiment.getWeight());
-            int candidateScore = prevScoreBack + experiment.getRating();
-            int prevScoreAbove = above(table, row, weight);
-            table[row][weight] = Integer.max(candidateScore, prevScoreAbove);
-        }
-    }
-
-    private static int getPrevScore(int[][] table, int row, int col, int weight) {
-        if(row == 0)
-            return 0;
-
-        if(weight > col)
-            return 0;
-
-        return table[row-1][col - weight];
-    }
-
-    private static int above(int[][] table, int row, int col) {
+    /**
+     * Helper method to safely get the above cell, or return 0 if out of bounds
+     * @param table table to undex into
+     * @param row row of cell to look above
+     * @param col column of cell to index into
+     * @return the value of the cell above the current one (or 0)
+     */
+    private static int getAbove(int[][] table, int row, int col) {
         if(row == 0)
             return 0;
         return table[row-1][col];
     }
 
-    private static int left(int[][] table, int row, int col, int weight) {
-        if(weight >= col)
-            return 0;
-        return table[row][col - weight];
+    /**
+     * Helper method to diagnose issues with the DP table.
+     * Not necessary for end product.
+     * 
+     * Prints the table of integers
+     * @param table table of integers to be printed.
+     */
+    @SuppressWarnings("unused")
+    private static void printTable(int[][] table) {
+        for(int[] row : table) {
+            System.out.printf("\n[ ");
+            for(int cell : row) {
+                System.out.printf("%d\t", cell);
+            }
+            System.out.printf("]");
+        }
+        System.out.println("\n");
     }
 }

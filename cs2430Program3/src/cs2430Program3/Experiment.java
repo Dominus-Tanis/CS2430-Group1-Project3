a3f93d34668be353d1ebb04c3e5e09176e271241
CoryNeilsen <coryneilsen@gmail.com>
2026-04-10 08:56:04 -0600

    Added full comments for Experiment and Payload classes.

--- cs2430Program3/src/cs2430Program3/Experiment.java
+++ cs2430Program3/src/cs2430Program3/Experiment.java
@@ -4,35 +4,67 @@ package cs2430Program3;
  * Group 1 Cory Neilsen, Spencer Peck, Benjamin Shaw, Dallin Yauney CS 2430-002
  * Project 3: Optimal Selection – Spring 2026
  *
+ * The Experiment class is used to create Experiment objects and track their
+ * name, weight, rating, and score.
+ *
  * @author Cory
  */
 public class Experiment {
 
-    private String name;
-    private int weight;
-    private int rating;
-    private double score;
+	private String name;
+	private int weight;
+	private int rating;
+	private double score;
 
-    public Experiment(String name, int weight, int rating) {
-        this.name = name;
-        this.weight = weight;
-        this.rating = rating;
-        this.score = (double) rating / weight;
-    }
+	/**
+	 * Constructs an Experiment object using an input name, weight, and rating.
+	 * Score is calculated by dividing rating by weight.
+	 * 
+	 * @param name   Name of the experiment.
+	 * @param weight Weight of the experiment.
+	 * @param rating Rating of the experiment.
+	 */
+	public Experiment(String name, int weight, int rating) {
+		this.name = name;
+		this.weight = weight;
+		this.rating = rating;
+		this.score = (double) rating / weight;
+	}
 
-    public String getName() {
-        return name;
-    }
+	/**
+	 * A simple getter method to retrieve the name of an Experiment object.
+	 * 
+	 * @return Experiment name.
+	 */
+	public String getName() {
+		return name;
+	}
 
-    public int getWeight() {
-        return weight;
-    }
+	/**
+	 * A simple getter method to retrieve the weight of an Experiment object.
+	 * 
+	 * @return Experiment weight.
+	 */
+	public int getWeight() {
+		return weight;
+	}
 
-    public int getRating() {
-        return rating;
-    }
+	/**
+	 * A simple getter method to retrieve the rating of an Experiment object.
+	 * 
+	 * @return Experiment rating.
+	 */
+	public int getRating() {
+		return rating;
+	}
 
-    public double getScore() {
-        return score;
-    }
+	/**
+	 * A simple getter method to retrieve the score of an Experiment object. Score
+	 * is calculated by dividing rating by weight on Experiment object creation.
+	 * 
+	 * @return Experiment score.
+	 */
+	public double getScore() {
+		return score;
+	}
 }
--- cs2430Program3/src/cs2430Program3/Payload.java
+++ cs2430Program3/src/cs2430Program3/Payload.java
@@ -7,50 +7,84 @@ import java.util.List;
  * Group 1 Cory Neilsen, Spencer Peck, Benjamin Shaw, Dallin Yauney CS 2430-002
  * Project 3: Optimal Selection – Spring 2026
  *
+ * The Payload class provides functionality for a Payload object, which is a
+ * collection of Experiment objects that also tracks the combined weight and
+ * rating of all Experiments contained.
+ *
  * @author Cory
  */
 public class Payload {
 
-    private List<Experiment> experiments;
-    private int weight;
-    private int rating;
+	private List<Experiment> experiments;
+	private int weight;
+	private int rating;
 
-    public Payload() {
-        experiments = new ArrayList<>();
-    }
-    
-    public Payload(List<Experiment> externalExperimentList) {
-        experiments = new ArrayList<>();
-        for(Experiment experiment : externalExperimentList) {
-            experiments.add(experiment);
-            weight += experiment.getWeight();
-            rating += experiment.getRating();
-        }
-    }
-    
-    public String experimentList() {
-        String experimentList = "";
-        for (Experiment experiment : experiments) {
-            if (experimentList.isEmpty()){
-                experimentList += experiment.getName();
-            } else {
-                experimentList += ", " + experiment.getName();
-            }
-        }
-        return "Experiments: " + experimentList;
-    }
+	/**
+	 * Construct an empty Payload to add Experiments to.
+	 */
+	public Payload() {
+		experiments = new ArrayList<>();
+	}
 
-    public int getWeight() {
-        return weight;
-    }
+	/**
+	 * Constructor to build a Payload using an input list of experiments.
+	 * 
+	 * @param externalExperimentList A list of Experiment objects used to build the
+	 *                               Payload.
+	 */
+	public Payload(List<Experiment> externalExperimentList) {
+		experiments = new ArrayList<>();
+		for (Experiment experiment : externalExperimentList) {
+			experiments.add(experiment);
+			weight += experiment.getWeight();
+			rating += experiment.getRating();
+		}
+	}
 
-    public int getRating() {
-        return rating;
-    }
+	/**
+	 * Get the names of all Experiments in the Payload.
+	 * 
+	 * @return A String output of the names of all Experiments contained within the
+	 *         Payload.
+	 */
+	public String experimentList() {
+		String experimentList = "";
+		for (Experiment experiment : experiments) {
+			if (experimentList.isEmpty()) {
+				experimentList += experiment.getName();
+			} else {
+				experimentList += ", " + experiment.getName();
+			}
+		}
+		return "Experiments: " + experimentList;
+	}
 
-    public void addExperiment(Experiment experiment) {
-        experiments.add(experiment);
-        weight += experiment.getWeight();
-        rating += experiment.getRating();
-    }
+	/**
+	 * Get the combined weight of Experiments in the Payload.
+	 * 
+	 * @return An int value of the total weight of the Payload.
+	 */
+	public int getWeight() {
+		return weight;
+	}
+
+	/**
+	 * Get the combined rating of Experiments in the Payload.
+	 * 
+	 * @return An int value of the total rating of the Payload.
+	 */
+	public int getRating() {
+		return rating;
+	}
+
+	/**
+	 * Add an Experiment to the Payload.
+	 * 
+	 * @param experiment Experiment object to be added to the Payload.
+	 */
+	public void addExperiment(Experiment experiment) {
+		experiments.add(experiment);
+		weight += experiment.getWeight();
+		rating += experiment.getRating();
+	}
 }
